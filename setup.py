01266db4b3fed80c04340cbf7c7c2d794a09df5b
Iikka Hauhio <iikka@kaivos.org>
2018-04-02 20:53:37 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name='suomilog',
+	version='1.0',
+	description='Collection of natural language parsing utilities',
+	url='http://github.com/fergusq/suomilog',
+	author='Iikka Hauhio',
+	author_email='iikka.hauhio@gmail.com',
+	license='GPL',
+	classifiers=[
+		'Programming Language :: Python :: 3'
+	],
+	packages=['suomilog'],
+	python_requires='>=3',
+	install_requires=['voikko'],
+)
